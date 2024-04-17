994a0472df02c79c40665dee1daa995c72da1290
Nurali <nurali.shamel@icloud.com>
2024-04-17 19:31:52 +0500

    Initial Commit

---
+++ FireBaseLessonN1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseLessonN1
+//
+//  Created by Nurali Shamel on 17.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
