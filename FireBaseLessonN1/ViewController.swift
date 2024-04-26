e436a81e8e5f0ce7447d83579aa77682e63e3521
Nurali <nurali.shamel@icloud.com>
2024-04-27 01:18:26 +0500

    first commit

--- FireBaseLessonN1/ViewController.swift
+++ FireBaseLessonN1/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  FireBaseLessonN1
-//
-//  Created by Nurali Shamel on 17.04.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
